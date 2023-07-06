2889a1de4b1b49b7a9a8fcc1726b63cd4fad6df1
souravcrj5495 <souravcrj1995@gmail.com>
2023-07-06 23:23:01 +0530

    filter

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var listGroupItem = document.getElementsByClassName('list-group-item');
-console.log(itemList);
+var submit = document.getElementById('addSubmit');
+var desCription = document.createElement('input');
+desCription.id = 'd';
+console.log(desCription);
+// var des = desCription.setAttribute('id', 'description');
+console.log(document.getElementById('d'));
+form.insertBefore(desCription, submit);
+
+// console.log(itemList);
+var filter = document.getElementById('filter');
 
 for(let i=0; i<listGroupItem.length; i++){
     var editBtn = document.createElement('button');
@@ -29,15 +38,19 @@ function addItem(e){
 
     //Get input value
     var newItem = document.getElementById('item').value;
+    var newItem2 = document.getElementById('d').value;
+    
 
     //Create new li Element
     var li = document.createElement('li');
 
     //add class
     li.className = 'list-group-item';
-
+    
     //Add text node with input value
+    
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newItem2));
 
     //Create del button element
     var deleteBtn = document.createElement('button');
@@ -85,3 +98,29 @@ let removeItem = (e)=>{
   }
 
 itemList.addEventListener('click',removeItem);
+
+
+
+//Filter Items
+function filterItems(e){
+  //convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  //Get lis
+  var items = itemList.getElementsByTagName('li');
+  //Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    var description = item.childNodes[1].textContent;
+    console.log(itemName);
+    if(itemName.toLowerCase().indexOf(text) != -1||description.toLowerCase().indexOf(text)!= -1){
+      item.style.display = 'block';
+
+    }
+    else {
+      item.style.display = 'none';
+    }
+  })
+}
+//Filter Event
+filter.addEventListener('keyup', filterItems);
+
